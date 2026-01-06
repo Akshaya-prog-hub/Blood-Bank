1641675d041c58e5865e8b994ca62a9dc001e4d2
Akshayasenthilkumar-08 <itaakshaya29@gmail.com>
2026-01-06 19:28:17 +0530

    firebase env setup and config changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWNYS3QgJJJ8PFwoGsm1C4IupPwA2mekU",
-  authDomain: "bloodbank-89899.firebaseapp.com",
-  projectId: "bloodbank-89899",
-  storageBucket: "bloodbank-89899.appspot.com", // ✅ FIX
-  messagingSenderId: "264319335796",
-  appId: "1:264319335796:web:0719a8816b56f4d6fcc021",
-  measurementId: "G-LPBCE1RJG8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,3 +18,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+export default app;
